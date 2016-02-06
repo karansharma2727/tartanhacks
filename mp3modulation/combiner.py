b0d8dc3f30f3a9676cd6e98e290c2b4fab1cba21
Jichao Sun <Jichao@MBA.wv.cc.cmu.edu>
2016-02-06 01:41:28 -0500

    simple nextSong

--- mp3modulation/combiner.py
+++ mp3modulation/combiner.py
@@ -38,3 +38,33 @@ def combinemix(songarr, fadeoutarr):
         newseg = newseg.append(songarr[index+1], crossfade=fadeoutarr[index+1])
 
     return newseg
+
+def nextSong(s1, s2):
+    (sb1, bar1, d1, b1, eb1) = s1
+    (sb2, bar2, d2, b2, eb2) = s2
+
+    # Get time for a single bar in song 1
+    barLen1 = (d1 - bar1)
+
+    # Check how many bars are in breakdown in song 1
+    barsToEB1 = (b1 - eb1) / barLen1
+    
+    # Bars in build up of song 2
+    barLen2 = (d2 - bar2)
+    barsToD2 = (sb - d2) / barLen2
+
+    # Build up time matches break down time (or breakdown is longer)
+    # Just put the build during break down.
+    if round(barsToEB1) <= round(barsToD2):
+        startTime = sb2
+        endTime = None
+        fadeOut = sb2 - d2
+
+    # Build up time less than break down time.
+    # Just mix in D2 after fixing bars
+    elif barsToEB1 < barsToD2:
+        offset = (d2 - sb2) - (eb1 - b1)
+        startTime = sb2 + offset
+        endTime = None
+        fadeOut = eb1 - b1
+    return (starTime, endTime, fadeOut)
